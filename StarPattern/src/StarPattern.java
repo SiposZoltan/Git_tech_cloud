0e69ce327f76b57049ecb38cb7e69024ce763684
Desktop <techbogart@gmail.com>
2017-10-12 17:08:31 +0300

    Dice/Zaruri configurabile (numar de fete, numar de zaruri, numar de aruncari).

---
+++ StarPattern/src/StarPattern.java
@@ -0,0 +1,15 @@
+//public class StarPattern {
+//    public static void main(String[] args) {
+//        String star = "*";
+//
+//        System.out.println(star);
+//        System.out.println(star + star + star);
+//        System.out.println(star + star + star + star + star);
+//        System.out.println(star + star + star + star + star + star + star);
+//        System.out.println(star + star + star + star + star);
+//        System.out.println(star + star + star);
+//        System.out.println(star);
+//    }
+//
+//
+//}
