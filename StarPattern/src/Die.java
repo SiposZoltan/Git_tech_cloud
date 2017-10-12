50d6ccdfdde868f3a57bac576fcb6ccc1e8911e4
Desktop <techbogart@gmail.com>
2017-10-12 19:10:09 +0300

    Adaugat commenturi.

--- StarPattern/src/Die.java
+++ StarPattern/src/Die.java
@@ -1,28 +1,32 @@
 import java.util.Random;
 
 /*
-Program de dat cu zarurile,
+Sipos Zoltan 12 Oct 2017.
+Acesta este un program de dat cu zarurile.
+Se pot configura numarul de zaruri, de fete si de cate ori se pot "arunca" acestea.
  */
 
-public class Die {
+public class Die {  //Clasa Die unde simulam un zar.
 
-    private int sides;
-    private int value;
+    private int sides;  //Laturile zarului.
+    private int value;  //Valoarea laturii zarului.
 
-    public Die(int numSides) {
+    public Die(int numSides) { // Constructorul zarului.
         sides = numSides;
         roll();
     }
 
-    public void roll() {
-        Random rand = new Random();
-        value = rand.nextInt(sides)+1;
+    public void roll() {    //Metoda roll, simuleaza "aruncatul" zarului.
+        Random rand = new Random();  //Metoda roll utilizeaza un obiect de tip random.
+        value = rand.nextInt(sides) + 1; //Se populeaza "value" cu rand nr laturi (+ 1 deoarece incepe de la 0)
     }
 
-    public int getSides(){
+    public int getSides() {
         return sides;
-    }
-    public int getValue(){
+    } //getter de laturi .
+
+    public int getValue() {
         return value;
-    }
+    } //getter de valori.
+
 }
